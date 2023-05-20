35193489f1dba43cb88678c264d3f05fbc4d5b4a
Humberto CL <87952396+Arokeji@users.noreply.github.com>
2023-05-20 13:56:51 +0200

    Seed de mensajes y productos con sus relaciones correcto, a falta del seed de chat

--- seeds/product.seed.js
+++ seeds/product.seed.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const { connect } = require("../db.js");
 const { Product } = require("../Models/Product.js");
+const { User } = require("../Models/User.js");
+const { randomNumber } = require("../utils/randomNumber.js");
 
 const productSeed = async () => {
   try {
@@ -12,40 +14,56 @@ const productSeed = async () => {
     await Product.collection.drop();
     console.log("🧹 Productos eliminados correctamente");
 
+    // Listado de productos
     const productList = [
       {
         product: "Cicloste",
         price: 25,
-        description: "Cicloste con poco uso. Conservado en su caja"
+        description: "Cicloste con poco uso. Conservado en su caja",
       },
       {
         product: "Crotolamo",
         price: 10,
-        description: "Se vende por no usar."
+        description: "Se vende por no usar.",
       },
       {
         product: "Permatrago",
         price: 50,
-        description: "Con factura y garantia"
+        description: "Con factura y garantia",
       },
       {
         product: "Uxiono",
         price: 45,
-        description: "Precio negociable"
+        description: "Precio negociable",
       },
       {
         product: "Cicloste",
         price: 25,
-        description: "Cicloste con poco uso. Conservado en su caja"
+        description: "Cicloste con poco uso. Conservado en su caja",
       },
     ];
+
+    // Recuperacion de usuarios para asignarlos a productos
+    const allUsers = await User.find();
+
+    if (!allUsers.length) {
+      console.log("🏜️ No hay usuarios en la base de datos, no se pueden relacionar productos.");
+      return;
+    }
+
+    // Asigna un usuario aleatorio a cada producto
+    for (let i = 0; i < productList.length; i++) {
+      const product = productList[i];
+      const randomUser = allUsers[randomNumber(0, allUsers.length - 1)];
+      product.seller = randomUser._id;
+    }
 
     // Insercion de productos
     const documents = productList.map((product) => new Product(product));
     await Product.insertMany(documents);
-    console.log("✅ Productos agregados correctamente.")
+    console.log("✅ Productos agregados correctamente.");
   } catch (error) {
-    console.error(error)
+    console.error(error);
   } finally {
     mongoose.disconnect();
   }
---
+++ utils/randomNumber.js
@@ -0,0 +1,11 @@
+// Generacion de numero aleatorio
+const randomNumber = (min = 0, max = 100) => {
+  const difference = max - min;
+  let rand = Math.random();
+  rand = Math.floor(rand * difference);
+  rand = rand + min;
+
+  return rand;
+};
+
+module.exports = { randomNumber };
