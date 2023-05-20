6cf9dc3050d18e7489eb9a5477fa6c015a7ccc24
Karl <karlbrandon9@gmail.com>
2023-05-20 11:00:42 +0200

    Modelos product, chat y message

---
+++ Models/Product.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const { User } = require("./User.js");
+
+const productSchema = new Schema(
+  {
+    product: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    pictures: {
+      type: String,
+      required: true,
+    },
+    buyer: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: User,
+    },
+    seller: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: User,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+module.exports = { Product };
